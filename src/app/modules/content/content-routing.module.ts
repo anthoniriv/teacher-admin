ae4fd83814f620ff559a67a4fe98e8c1f8bd5d38
MAURO <maurosalas153117@gmail.com>
2024-03-05 21:20:45 -0500

    content

--- src/app/modules/content/content-routing.module.ts
+++ src/app/modules/content/content-routing.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentComponent } from './content.component';
 import { MacawsComponent } from './components/macaws/macaws.component';
+import { ParrotsComponent } from './components/parrots/parrots.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ContentComponent,
+  },
+  { 
+    path: 'macaws', 
+    component: MacawsComponent 
+  },
+  { 
+    path: 'parrots', 
+    component: ParrotsComponent 
   },
 ];
 
